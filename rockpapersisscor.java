da042d2f4dd74b35e12b54830aa0f3a299933901
AmanS369 <91814909+AmanS369@users.noreply.github.com>
2021-10-04 11:37:57 +0530

    Update rockpapersisscor.java

--- rockpapersisscor.java
+++ rockpapersisscor.java
@@ -11,21 +11,21 @@ Random ra = new Random();
  System.out.println("select your side");
   System.out.println(" 1-> Rock");
   System.out.println("2-> Sisscor");
-  System.out.println("3-> paper");
+  System.out.println("3 -> paper");
   
     int a = sc.nextInt(3) ;
     switch (a) {
-    case 1-> System.out.println("you selected Rock");
-    case 2-> System.out.println("you selected Sisscor");
-    case 3-> System.out.println("you selected Paper"); } 
+	    case 1 : System.out.println("you selected Rock");
+	    case 2 : System.out.println("you selected Sisscor");
+	    case 3 : System.out.println("you selected Paper"); } 
    // default -> System.out.println("Play Peacefully");}
     
     System.out.println("Now Computer will Select his side");
     int b = ra.nextInt(3);
     		switch (b) {
-    	    case 1-> System.out.println("Computer selected Rock");
-    	    case 2-> System.out.println("Computer selected Sisscor");
-    	    case 3-> System.out.println("Computer selected Paper"); }
+			case 1 : System.out.println("Computer selected Rock");
+			case 2 :System.out.println("Computer selected Sisscor");
+			case 3 : System.out.println("Computer selected Paper"); }
     		
     	if (a==b) {System.out.println("Match draw");}
     	else if (a==1 && b==2) {System.out.println("PLayer win");}
